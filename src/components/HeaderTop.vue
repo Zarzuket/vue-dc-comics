ad3deefc631595a70b55cc238f0afe282d0f9d8a
Zarzuket <davide.bajo@gmail.com>
2021-09-29 15:09:27 +0200

    add imgs -  scaffolding HeaderTop common.scss

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <div>
+          <img src="" alt="">
+      </div>
+      <nav>
+          <ul>
+              <li>
+                  ciao
+              </li>
+          </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderTop",
+}
+</script>
+
+<style scoped>
+li{
+    list-style: none;
+}
+</style>
