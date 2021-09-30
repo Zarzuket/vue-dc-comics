ea05abba142220e53e3043921f346637d42f77ac
Zarzuket <davide.bajo@gmail.com>
2021-09-30 09:17:31 +0200

    adding footer bot

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <MainTop/>
     <ShopMenu/>
     <FooterTop/>
+    <FooterBot/>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import Header from "./components/Header.vue"
 import MainTop from "./components/MainTop.vue"
 import ShopMenu from "./components/ShopMenu.vue"
 import FooterTop from "./components/FooterTop.vue"
+import FooterBot from "./components/FooterBot.vue"
 export default {
   name: 'App',
   components: {
     Header,
     MainTop,
     ShopMenu,
-    FooterTop
+    FooterTop,
+    FooterBot
   }
 }
 </script>
---
+++ src/components/FooterBot.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <div class="background">
+        <div class="container">
+              <button class="btn">SIGN-UP NOW!</button>
+            <div class="container_ico">
+                <ul>
+                    <li v-for="(ico,index) in icos" :key="index">
+                        <img :src="'../img/' + ico.name + '.png'" alt="">
+                    </li>
+                </ul>              
+            </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"FooterBot",
+    data(){
+        return{
+            icos:[
+                {
+                    name:"footer-facebook"
+                },
+                {
+                    name:"footer-persicope"
+                },
+                {
+                    name:"footer-pinterest"
+                },
+                {
+                    name:"footer-twitter"
+                },
+                {
+                    name:"footer-youtube"
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/variables";
+
+.background{
+    background-color: #303030;
+    height: 12.5rem;
+}
+.btn{
+    padding: 10px;
+    border:3px solid $primarycolor;
+    background-color: #303030;
+    color: white;
+    font-size: 1.25rem;
+}
+</style>
