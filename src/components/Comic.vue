527c8065294d6df4d579b09c1c043110bac37be4
Zarzuket <davide.bajo@gmail.com>
2021-09-30 18:11:14 +0200

    add probs

---
+++ src/components/Comic.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="card">
+      <img class="card_img" :src="com.thumb" alt="com.series">
+      <div class="card_title">
+        {{com.series}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Comic",
+  props:["com"]
+}
+</script>
+
+<style lang="scss" scoped>
+.card{
+    padding: 5px;
+    width: calc(100% / 6);
+		&_title{
+			text-align: center;
+			color: white;
+		}
+		&_img{
+			width: 100%;
+		}
+}
+</style>
